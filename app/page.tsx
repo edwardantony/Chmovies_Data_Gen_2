714daafaf011012aa581d95cf3b49aecb28175b9
priyeshantony <priyeshantony@gmail.com>
2025-04-01 22:35:10 +0530

    update

--- app/page.tsx
+++ app/page.tsx
@@ -13,10 +13,10 @@ Amplify.configure(outputs);
 const client = generateClient<Schema>();
 
 export default function App() {
-  const [todos, setTodos] = useState<Array<Schema["CH_Gen2_Genre"]["type"]>>([]);
+  const [todos, setTodos] = useState<Array<Schema["Todo"]["type"]>>([]);
 
   function listTodos() {
-    client.models.CH_Gen2_Genre.observeQuery().subscribe({
+    client.models.Todo.observeQuery().subscribe({
       next: (data) => setTodos([...data.items]),
     });
   }
@@ -26,7 +26,7 @@ export default function App() {
   }, []);
 
   function createTodo() {
-    client.models.CH_Gen2_Genre.create({
+    client.models.Todo.create({
       content: window.prompt("Todo content"),
     });
   }
