fd73697de61dfdfa05dd33e7e528bb3731c488f8
priyeshantony <priyeshantony@gmail.com>
2025-04-02 14:00:40 +0530

    pages

---
+++ amplify/data/resource.ts
@@ -0,0 +1,23 @@
+import { type ClientSchema, a, defineData } from "@aws-amplify/backend";
+
+const schema = a.schema({
+  Todo: a
+    .model({
+      content: a.string(),
+    })
+    .authorization((allow) => [allow.publicApiKey()]),
+});
+
+export type Schema = ClientSchema<typeof schema>;
+
+export const data = defineData({
+  schema,
+  authorizationModes: {
+    defaultAuthorizationMode: "apiKey",
+    // API Key is used for a.allow.public() rules
+    apiKeyAuthorizationMode: {
+      expiresInDays: 30,
+    },
+  },
+});
+
--- pages/index.tsx
+++ pages/index.tsx
@@ -1,14 +1,6 @@
-"use client";
-
 import { useState, useEffect } from "react";
 import { generateClient } from "aws-amplify/data";
 import type { Schema } from "@/amplify/data/resource";
-import "./../app/app.css";
-import { Amplify } from "aws-amplify";
-import outputs from "@/amplify_outputs.json";
-import "@aws-amplify/ui-react/styles.css";
-
-Amplify.configure(outputs);
 
 const client = generateClient<Schema>();
 
@@ -43,7 +35,7 @@ export default function App() {
       <div>
         🥳 App successfully hosted. Try creating a new todo.
         <br />
-        <a href="https://docs.amplify.aws/nextjs/start/quickstart/nextjs-app-router-client-components/">
+        <a href="https://docs.amplify.aws/gen2/start/quickstart/nextjs-pages-router/">
           Review next steps of this tutorial.
         </a>
       </div>
