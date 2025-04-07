bf6ecec0d041998417d8fe263d0338daa1922144
priyeshantony <priyeshantony@gmail.com>
2025-04-08 02:07:35 +0530

    updates

--- app/logout/page.tsx
+++ app/logout/page.tsx
@@ -1,2 +1,10 @@
-import Logout from '@/app/components/auth/AuthSignout';
-export default Logout;
+// app/logout/page.tsx
+import dynamic from 'next/dynamic';
+
+const Logout = dynamic(() => import('@/app/components/auth/AuthSignout'), {
+  ssr: true,
+});
+
+export default function LogoutPage() {
+  return <Logout />;
+}
