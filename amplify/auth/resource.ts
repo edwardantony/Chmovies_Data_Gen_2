3578d20bd59ca8267f9f5fac39f670014aeb9657
priyeshantony <priyeshantony@gmail.com>
2025-04-08 22:21:34 +0530

    P

--- amplify/auth/resource.ts
+++ amplify/auth/resource.ts
@@ -1,4 +1,4 @@
-import { defineAuth } from "@aws-amplify/backend";
+import { defineAuth } from '@aws-amplify/backend';
 
 /**
  * Define and configure your auth resource
@@ -7,5 +7,10 @@ import { defineAuth } from "@aws-amplify/backend";
 export const auth = defineAuth({
   loginWith: {
     email: true,
+    phone: true,
   },
+  
+  
+  
+  
 });
