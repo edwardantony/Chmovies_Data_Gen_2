354c9245de9e3cf210fd6956218cafd220434c07
priyeshantony <priyeshantony@gmail.com>
2025-04-09 11:22:10 +0530

    Update resource.ts

--- amplify/auth/resource.ts
+++ amplify/auth/resource.ts
@@ -5,10 +5,6 @@ export const auth = defineAuth({
     email: true,
     phone: true,
   },
-  userAttributes: {
-    email: { required: true },
-    phoneNumber: { required: false },
-  },
   multifactor: {
     mode: 'OPTIONAL',
     sms: true,
