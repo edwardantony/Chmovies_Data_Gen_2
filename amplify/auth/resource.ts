65ba89cf233d081afb115e39ba48abe3d5ba28c3
priyeshantony <priyeshantony@gmail.com>
2025-04-09 08:22:09 +0530

    Neat

--- amplify/auth/resource.ts
+++ amplify/auth/resource.ts
@@ -1,18 +1,11 @@
-// amplify/auth/resource.ts
-import { defineAuth } from '@aws-amplify/backend';
+import { defineAuth } from "@aws-amplify/backend";
 
+/**
+ * Define and configure your auth resource
+ * @see https://docs.amplify.aws/gen2/build-a-backend/auth
+ */
 export const auth = defineAuth({
   loginWith: {
     email: true,
-    phone: true,
-  },
-  userAttributes: {
-    email: { required: true },
-    phoneNumber: { required: false },
-  },
-  multifactor: {
-    mode: 'OPTIONAL',
-    sms: true,
-    totp: true,
   },
 });
